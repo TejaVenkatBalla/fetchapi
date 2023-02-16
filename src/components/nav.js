645fef45d4064e9dfca1c4607bbb4c63d4fb9827
TejaVenkatBalla <124157073+TejaVenkatBalla@users.noreply.github.com>
2023-02-16 12:30:56 +0530

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,9 +4,9 @@ function Navbar() {
   return (
     <nav>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="https://www.google.com/">Home</a></li>
+        <li><a href="https://www.google.com/">About</a></li>
+        <li><a href="https://www.google.com/">Contact</a></li>
       </ul>
     </nav>
   );
